cf005adbd0fa4b0154b81cbd11a1b04000fb478f
Benjamin Tobler <benjamin.tobler@bluewin.ch>
2020-02-03 11:59:17 +0100

    Update project_list.py

--- project_list.py
+++ project_list.py
@@ -1,6 +1,13 @@
+import json
 class project_list:
-    projectlist = []
     def __init__(self):
-        self.data = []
+        with open("projects.txt", "r") as read_file:
+            self.data = json.load(read_file)
     def add_project(self, project):
-        self.projectlist.append(project)
+        self.data["data"].append(project)
+
+with open("projects.txt", "r") as read_file:
+    data = json.load(read_file)
+
+#for testing 
+print(data["data"][0]["monument_id"])
